fe85b8da8f51697c4db3be1b3554c4bc6185185b
yanxxavier <yanxavierlindolfo@gmail.com>
2024-06-03 21:24:26 -0300

    Feat Services reponsive

--- assets/js/modules/servicos.js
+++ assets/js/modules/servicos.js
@@ -2,3 +2,34 @@ export default function initServicos() {
 
 }
 
+var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 0,
+      modifier: 0,
+      slideShadows: true,
+      scale : 0,
+    },
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+        },
+        950: {
+            slidesPerView: 2,
+          },
+      },
+  });
+
+
+
